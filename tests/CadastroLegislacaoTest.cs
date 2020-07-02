c53c26763075a2ecea85942269e5ed85896468a5
lucasramon <lucas.ramon.jc@gmail.com>
2020-07-02 15:21:43 -0300

    implementando caso de teste para erro de upload de arquivo

--- tests/CadastroLegislacaoTest.cs
+++ tests/CadastroLegislacaoTest.cs
@@ -116,8 +116,141 @@ namespace LegisTests
             FindBy(Selector.ID, "idEntrarLogin").Click();
         }
         
+        //[Test]
+        // public void testeBemSucedido()
+        // {
+        //     Login();
+            
+        //     //* go to module
+        //     FindBy(Selector.LINK, "Módulo de controle de legislações do Legis").Click();
+        //     FindBy(Selector.ID, "LegislacaoCadastro").Click();
+
+        //     Thread.Sleep(5000);
+
+        //     //* Select Esfera
+        //     // FindBy(Selector.XPATH,"(//input[@type='text'])[3]").Click();
+        //     // FindBy(Selector.XPATH, "/html/body/app-root/app-dashboard/div/div/main/app-legislacao-cadastro/div/fieldset/form/div[1]/div[1]/ng-select/ng-dropdown-panel/div/div[2]/div[1]").Click();
+
+
+        //     FindBy(Selector.ID, "idEsferaGovernamental").Click();
+        //     FindBy(Selector.XPATH, "/html/body/app-root/app-dashboard/div/div/main/app-legislacao-cadastro/div/fieldset/form/div[1]/div[1]/ng-select/ng-dropdown-panel/div/div[2]/div[1]").Click();
+            
+        //     //* Select Area Atuacao
+        //     IWebElement areaAtuacao = FindBy(Selector.ID, "select_idAreaAtuacao");
+        //     if(areaAtuacao != null){
+        //     areaAtuacao.Click();    
+        //     FindBy(Selector.XPATH, "/html/body/app-root/app-dashboard/div/div/main/app-legislacao-cadastro/div/fieldset/form/div[1]/div[2]/ng-select/ng-dropdown-panel/div/div[2]/div[1]").Click();
+        //     }
+        //     //  FindBy(Selector.ID, "select_idAreaAtuacao").Click();
+        //     //  FindBy(Selector.XPATH, "/html/body/app-root/app-dashboard/div/div/main/app-legislacao-cadastro/div/fieldset/form/div[1]/div[2]/ng-select/ng-dropdown-panel/div/div[2]/div[1]").Click();
+
+        //     //* Select Municipio
+        //     IWebElement cidade = FindBy(Selector.ID, "idCidade");
+        //     if(cidade !=null){
+        //     FindBy(Selector.ID, "idCidade").Click();
+        //     FindBy(Selector.XPATH, "/html/body/ng-dropdown-panel/div/div[2]/div[88]").Click();
+
+        //     }
+            
+        //     //FindBy(Selector.ID, "idCidade").Click();
+        //     //FindBy(Selector.XPATH, "/html/body/ng-dropdown-panel/div/div[2]/div[88]").Click();
+
+        //     Thread.Sleep(2000);
+
+        //     //* Select tipo norma
+        //     FindBy(Selector.ID, "select_idTipoNorma").Click();
+        //     FindBy(Selector.XPATH, "/html/body/app-root/app-dashboard/div/div/main/app-legislacao-cadastro/div/fieldset/form/div[2]/div[1]/ng-select/ng-dropdown-panel/div/div[2]/div[3]").Click();
+
+        //     //* Select Ano
+        //     IWebElement anoNorma = FindBy(Selector.ID, "anoNorma");
+        //     if(anoNorma != null){
+        //         anoNorma.Click();
+        //         anoNorma.Clear();
+        //         anoNorma.SendKeys("2019");
+        //     }
+
+        //     //* Select Numero
+        //     IWebElement numeroNorma = FindBy(Selector.ID, "numeroNorma");
+        //     if(numeroNorma != null){
+        //         numeroNorma.Click();
+        //         numeroNorma.Clear();
+        //         numeroNorma.SendKeys("0123456789");
+        //     }
+
+        //     //* Select Meio Publicacao
+        //     FindBy(Selector.ID, "select_idMeioPublicacao").Click();
+        //     FindBy(Selector.XPATH, "/html/body/app-root/app-dashboard/div/div/main/app-legislacao-cadastro/div/fieldset/form/div[3]/div[1]/ng-select/ng-dropdown-panel/div/div[2]/div[3]").Click();
+
+        //     //* Select Data Publicacao
+        //     IWebElement dataPublicacao = FindBy(Selector.ID, "dataPublicacao");
+        //     dataPublicacao.Click();
+        //     dataPublicacao.Clear();
+        //     dataPublicacao.SendKeys("01/01/1950");
+
+        //     //* Select Data Inicio Vigencia
+        //     IWebElement dataInicioVigencia = FindBy(Selector.ID, "dataInicioVigencia");
+        //     if(dataInicioVigencia!=null){
+        //     dataInicioVigencia.Click();
+        //     dataInicioVigencia.Clear();
+        //     dataInicioVigencia.SendKeys("01/01/2020");
+        //     }
+
+        //     //* Select Data Fim Vigencia
+        //     IWebElement dataFimVigencia = FindBy(Selector.ID, "dataFimVigencia");
+        //     if(dataFimVigencia!=null){
+        //     dataFimVigencia.Click();
+        //     dataFimVigencia.Clear();
+        //     dataFimVigencia.SendKeys("01/12/2020");
+        //     }
+
+        //     //add ementa
+        //     IWebElement ementa = FindBy(Selector.ID,"ementa");
+        //     if(ementa!=null){
+        //     ementa.Click();
+        //     ementa.Clear();
+        //     ementa.SendKeys("testando");
+        //     }
+
+        //     //add endereço eletronico
+        //     IWebElement enderecoEletronico = FindBy(Selector.ID,"enderecoEletronico");
+        //     if(enderecoEletronico!=null){
+        //     enderecoEletronico.Click();
+        //     enderecoEletronico.Clear();
+        //     enderecoEletronico.SendKeys("https://www.google.com.br/");
+        //     }
+
+            
+        //     //add assunto normas
+        //     for(int i=1;i<3;i++){
+        //     FindBy(Selector.ID, "idAssuntoNorma").Click();
+        //     FindBy(Selector.XPATH, "/html/body/app-root/app-dashboard/div/div/main/app-legislacao-cadastro/div/fieldset/form/div[5]/div[1]/ng-select/ng-dropdown-panel/div/div[2]/div["+i+"]").Click();
+        //     FindBy(Selector.ID, "acao").Click();
+        //     }
+
+        //     //add Orgaos
+        //      for(int i=1;i<3;i++){
+        //      FindBy(Selector.ID, "idOrgao").Click();
+        //      FindBy(Selector.XPATH, "/html/body/app-root/app-dashboard/div/div/main/app-legislacao-cadastro/div/fieldset/form/div[5]/div[3]/ng-select/ng-dropdown-panel/div/div[2]/div["+i+"]").Click();
+        //      FindBy(Selector.ID, "acaoOrgao").Click();
+        //      }
+
+        //     //* Select pdf
+        //     IWebElement anexo = FindBy(Selector.ID, "anexo");
+        //     // anexo.Click();
+        //     // anexo.Clear();
+        //     anexo.SendKeys(@"/home/user/teste/teste.pdf");
+
+        //     //* Enviar
+        //     FindBy(Selector.XPATH, "//*[@id='legislacaoCadastroForm']/div[8]/button").Click();
+        //     Thread.Sleep(200);
+        //     Assert.AreEqual(FindBy(Selector.ID,"swal2-title").Text, "Sucesso");
+            
+        // }
+
+
+
         [Test]
-        public void TheUntitledTestCaseTest()
+        public void testeFalhaFormatoArquivo()
         {
             Login();
             
@@ -200,7 +333,7 @@ namespace LegisTests
             if(dataFimVigencia!=null){
             dataFimVigencia.Click();
             dataFimVigencia.Clear();
-            dataFimVigencia.SendKeys("01/12/2020");
+            dataFimVigencia.SendKeys("01/03/2020");
             }
 
             //add ementa
@@ -238,13 +371,10 @@ namespace LegisTests
             IWebElement anexo = FindBy(Selector.ID, "anexo");
             // anexo.Click();
             // anexo.Clear();
-            anexo.SendKeys(@"/home/user/teste/teste.pdf");
+            anexo.SendKeys(@"/home/user/Concursos - Conceito do Sistema.docx");
 
-            //* Enviar
-            FindBy(Selector.XPATH, "//*[@id='legislacaoCadastroForm']/div[8]/button").Click();
-            Thread.Sleep(200);
-            Assert.AreEqual(FindBy(Selector.ID,"swal2-title").Text, "Sucesso");
             
+            Assert.AreEqual(FindBy(Selector.ID,"swal2-title").Text, "Erro.");
         }
         private bool IsElementPresent(By by)
         {
