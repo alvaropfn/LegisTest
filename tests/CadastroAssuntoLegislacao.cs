33424171cdef485ab48ccca6ec7d51cf0672e723
lucasramon <lucas.ramon.jc@gmail.com>
2020-07-03 14:37:01 -0300

    implementando teste de exclusao de uma linha da listagem de normas

--- tests/CadastroAssuntoLegislacao.cs
+++ tests/CadastroAssuntoLegislacao.cs
@@ -1,230 +1,230 @@
-using System;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using SeleniumExtras.WaitHelpers;
+// using System;
+// using System.Text;
+// using System.Text.RegularExpressions;
+// using System.Threading;
+// using NUnit.Framework;
+// using OpenQA.Selenium;
+// using OpenQA.Selenium.Chrome;
+// using OpenQA.Selenium.Support.UI;
+// using SeleniumExtras.WaitHelpers;
 
-namespace LegisTests
-{
-    [TestFixture]
-    public class CadastroAssuntoLegislacao
-    {
-        private IWebDriver driver;
-        public WebDriverWait wait;
-        private StringBuilder verificationErrors;
-        private bool acceptNextAlert = true;
+// namespace LegisTests
+// {
+//     [TestFixture]
+//     public class CadastroAssuntoLegislacao
+//     {
+//         private IWebDriver driver;
+//         public WebDriverWait wait;
+//         private StringBuilder verificationErrors;
+//         private bool acceptNextAlert = true;
 
-        private void LimparMemoria()
-        {
-            string comando;
-            comando = "tasklist | grep chromedriver";
-            System.Diagnostics.Process.Start("CMD.exe", comando);
+//         private void LimparMemoria()
+//         {
+//             string comando;
+//             comando = "tasklist | grep chromedriver";
+//             System.Diagnostics.Process.Start("CMD.exe", comando);
 
-            comando = "taskkill /F /IM chromedriver.exe /T";
-            System.Diagnostics.Process.Start("CMD.exe", comando);
-        }
+//             comando = "taskkill /F /IM chromedriver.exe /T";
+//             System.Diagnostics.Process.Start("CMD.exe", comando);
+//         }
 
-        [SetUp]
-        public void SetupTest()
-        {
-            //this.LimparMemoria();
-            driver = new ChromeDriver(ChromeDriverService.CreateDefaultService(Environment.CurrentDirectory));
+//         [SetUp]
+//         public void SetupTest()
+//         {
+//             //this.LimparMemoria();
+//             driver = new ChromeDriver(ChromeDriverService.CreateDefaultService(Environment.CurrentDirectory));
 
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            this.wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(10));
+//             driver.Manage().Window.Maximize();
+//             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+//             this.wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(10));
 
-            verificationErrors = new StringBuilder();
-        }
+//             verificationErrors = new StringBuilder();
+//         }
 
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
-            {
-                driver.Quit();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-            Assert.AreEqual("", verificationErrors.ToString());
-        }
+//         [TearDown]
+//         public void TeardownTest()
+//         {
+//             try
+//             {
+//                 driver.Quit();
+//             }
+//             catch (Exception)
+//             {
+//                 // Ignore errors if unable to close the browser
+//             }
+//             Assert.AreEqual("", verificationErrors.ToString());
+//         }
 
-        public enum Selector
-        {
-            ID,
-            CLASS,
-            CSS,
-            XPATH,
-            LINK,
-        }
+//         public enum Selector
+//         {
+//             ID,
+//             CLASS,
+//             CSS,
+//             XPATH,
+//             LINK,
+//         }
 
-        public IWebElement FindBy(Selector selector, string refs)
-        {
-            IWebElement toReturn;
-            try
-            {
-                switch (selector)
-                {
-                    case Selector.ID:
-                        toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.Id(refs)));
-                        break;
-                    case Selector.CLASS:
-                        toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.ClassName(refs)));
-                        break;
-                    case Selector.CSS:
-                        toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.CssSelector(refs)));
-                        break;
-                    case Selector.XPATH:
-                        // toReturn = wait.Until(ExpectedConditions.ElementExists(By.XPath(refs)));
-                        toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.XPath(refs)));
-                        break;
-                    case Selector.LINK:
-                        toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.LinkText(refs)));
-                        break;
-                    default:
-                        toReturn = null;
-                        break;
-                }
+//         public IWebElement FindBy(Selector selector, string refs)
+//         {
+//             IWebElement toReturn;
+//             try
+//             {
+//                 switch (selector)
+//                 {
+//                     case Selector.ID:
+//                         toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.Id(refs)));
+//                         break;
+//                     case Selector.CLASS:
+//                         toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.ClassName(refs)));
+//                         break;
+//                     case Selector.CSS:
+//                         toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.CssSelector(refs)));
+//                         break;
+//                     case Selector.XPATH:
+//                         // toReturn = wait.Until(ExpectedConditions.ElementExists(By.XPath(refs)));
+//                         toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.XPath(refs)));
+//                         break;
+//                     case Selector.LINK:
+//                         toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.LinkText(refs)));
+//                         break;
+//                     default:
+//                         toReturn = null;
+//                         break;
+//                 }
 
-            }
-            catch (System.Exception)
-            { toReturn = null; }
+//             }
+//             catch (System.Exception)
+//             { toReturn = null; }
 
-            Thread.Sleep(150);
-            return toReturn;
-        }
+//             Thread.Sleep(150);
+//             return toReturn;
+//         }
 
-        public void Login()
-        {
-            driver.Navigate().GoToUrl("http://localhost:4200/");
+//         public void Login()
+//         {
+//             driver.Navigate().GoToUrl("http://localhost:4200/");
 
-            IWebElement username = FindBy(Selector.ID, "username");
-            username.Click();
-            username.Clear();
-            username.SendKeys("05510151447");
+//             IWebElement username = FindBy(Selector.ID, "username");
+//             username.Click();
+//             username.Clear();
+//             username.SendKeys("05510151447");
 
-            IWebElement password = FindBy(Selector.ID, "password");
-            password.Click();
-            password.Clear();
-            password.SendKeys("tce@123");
+//             IWebElement password = FindBy(Selector.ID, "password");
+//             password.Click();
+//             password.Clear();
+//             password.SendKeys("tce@123");
 
-            FindBy(Selector.XPATH, "(//input[@type='text'])[2]").Click();
-            FindBy(Selector.XPATH, "/html/body/app-root/tce-login/div/form/div[3]/ng-select/ng-dropdown-panel/div/div[2]/div[1]").Click();
+//             FindBy(Selector.XPATH, "(//input[@type='text'])[2]").Click();
+//             FindBy(Selector.XPATH, "/html/body/app-root/tce-login/div/form/div[3]/ng-select/ng-dropdown-panel/div/div[2]/div[1]").Click();
 
-            FindBy(Selector.ID, "idEntrarLogin").Click();
-        }
-
-
-
-        [Test]
-        public void testeBemSucedido()
-        {
-            Login();
-
-            //* go to module
-            FindBy(Selector.LINK, "Módulo de administração do Legis").Click();
-            FindBy(Selector.ID, "AssuntoNorma").Click();
-
-            Thread.Sleep(1000);
-
-            FindBy(Selector.XPATH, "//div[@id='consulta']/a/i").Click();
+//             FindBy(Selector.ID, "idEntrarLogin").Click();
+//         }
 
 
-             IWebElement numeroNorma = FindBy(Selector.ID, "nomeAssuntoNorma");
-             if(numeroNorma != null){
-                 numeroNorma.Click();
-                 numeroNorma.Clear();
-                 numeroNorma.SendKeys("testando assunto norma");
-             }
 
-             FindBy(Selector.ID, "idButtonSalvar").Click();
-             Thread.Sleep(200);
-             Assert.AreEqual(FindBy(Selector.ID,"swal2-title").Text, "Sucesso");
+//         [Test]
+//         public void testeBemSucedido()
+//         {
+//             Login();
+
+//             //* go to module
+//             FindBy(Selector.LINK, "Módulo de administração do Legis").Click();
+//             FindBy(Selector.ID, "AssuntoNorma").Click();
+
+//             Thread.Sleep(1000);
+
+//             FindBy(Selector.XPATH, "//div[@id='consulta']/a/i").Click();
+
+
+//              IWebElement nomeAssuntoNorma = FindBy(Selector.ID, "nomeAssuntoNorma");
+//              if(nomeAssuntoNorma != null){
+//                  nomeAssuntoNorma.Click();
+//                  nomeAssuntoNorma.Clear();
+//                  nomeAssuntoNorma.SendKeys("testando assunto norma");
+//              }
+
+//              FindBy(Selector.ID, "idButtonSalvar").Click();
+//              Thread.Sleep(200);
+//              Assert.AreEqual(FindBy(Selector.ID,"swal2-title").Text, "Sucesso");
              
-        }
+//         }
 
 
-                [Test]
-        public void testeFalha()
-        {
-            Login();
+//                 [Test]
+//         public void testeFalha()
+//         {
+//             Login();
 
-            //* go to module
-            FindBy(Selector.LINK, "Módulo de administração do Legis").Click();
-            FindBy(Selector.ID, "AssuntoNorma").Click();
+//             //* go to module
+//             FindBy(Selector.LINK, "Módulo de administração do Legis").Click();
+//             FindBy(Selector.ID, "AssuntoNorma").Click();
 
-            Thread.Sleep(1000);
+//             Thread.Sleep(1000);
 
-            FindBy(Selector.XPATH, "//div[@id='consulta']/a/i").Click();
+//             FindBy(Selector.XPATH, "//div[@id='consulta']/a/i").Click();
 
-             string texto="";
-             IWebElement numeroNorma = FindBy(Selector.ID, "nomeAssuntoNorma");
-             if(numeroNorma != null){
-                 numeroNorma.Click();
-                 numeroNorma.Clear();
-                 numeroNorma.SendKeys(texto);
-             }
+//              string texto="";
+//              IWebElement nomeAssuntoNorma = FindBy(Selector.ID, "nomeAssuntoNorma");
+//              if(nomeAssuntoNorma != null){
+//                  nomeAssuntoNorma.Click();
+//                  nomeAssuntoNorma.Clear();
+//                  nomeAssuntoNorma.SendKeys(texto);
+//              }
 
-             FindBy(Selector.ID, "idButtonSalvar").Click();
-             Thread.Sleep(200);
-             Assert.AreEqual(FindBy(Selector.XPATH,"/html/body/app-root/app-dashboard/div/div/main/app-assunto-norma-form/tce-server-error-messages/div[1]/ul/li").Text,"Campo nome Assunto Norma obrigatório");
+//              FindBy(Selector.ID, "idButtonSalvar").Click();
+//              Thread.Sleep(200);
+//              Assert.AreEqual(FindBy(Selector.XPATH,"/html/body/app-root/app-dashboard/div/div/main/app-assunto-norma-form/tce-server-error-messages/div[1]/ul/li").Text,"Campo nome Assunto Norma obrigatório");
              
-        }
+//         }
 
 
 
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
+//         private bool IsElementPresent(By by)
+//         {
+//             try
+//             {
+//                 driver.FindElement(by);
+//                 return true;
+//             }
+//             catch (NoSuchElementException)
+//             {
+//                 return false;
+//             }
+//         }
 
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                this.driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
+//         private bool IsAlertPresent()
+//         {
+//             try
+//             {
+//                 this.driver.SwitchTo().Alert();
+//                 return true;
+//             }
+//             catch (NoAlertPresentException)
+//             {
+//                 return false;
+//             }
+//         }
 
-        private string CloseAlertAndGetItsText()
-        {
-            try
-            {
-                IAlert alert = this.driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
-        }
-    }
-}
+//         private string CloseAlertAndGetItsText()
+//         {
+//             try
+//             {
+//                 IAlert alert = this.driver.SwitchTo().Alert();
+//                 string alertText = alert.Text;
+//                 if (acceptNextAlert)
+//                 {
+//                     alert.Accept();
+//                 }
+//                 else
+//                 {
+//                     alert.Dismiss();
+//                 }
+//                 return alertText;
+//             }
+//             finally
+//             {
+//                 acceptNextAlert = true;
+//             }
+//         }
+//     }
+// }
--- tests/ListarLegislacaoTest.cs
+++ tests/ListarLegislacaoTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -7,93 +8,234 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Interactions;
 
-// namespace LegisTests
-// {
-//      [TestFixture]
-//     public class UntitledTestCase
-//     {
-//         private IWebDriver driver;
-//         private StringBuilder verificationErrors;
-//         private string baseURL;
-//         private bool acceptNextAlert = true;
-        
-//         [SetUp]
-//         public void SetupTest()
-//         {
-//             this.LimparMemoria();
-//             driver = new ChromeDriver(ChromeDriverService.CreateDefaultService(Environment.CurrentDirectory));
+namespace LegisTests
+{
+    [TestFixture]
+    public class ListarLegislacaoTest
+    {
 
-//             driver.Manage().Window.Maximize();
-//             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-//             this.wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(10));
 
-//             verificationErrors = new StringBuilder();
-//         }
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
 
-//         [TearDown]
-//         public void TeardownTest()
-//         {
-//             try
-//             {
-//                 driver.Quit();
-//             }
-//             catch (Exception)
-//             {
-//                 // Ignore errors if unable to close the browser
-//             }
-//             Assert.AreEqual("", verificationErrors.ToString());
-//         }
+        public WebDriverWait wait;
+        private string baseURL;
+        private bool acceptNextAlert = true;
 
-//         [Test]
-//         public void TheUntitledTestCaseTest()
-//         {
-//             driver.Navigate().GoToUrl("http://localhost:4200/#/dashboard/legislacaoconsulta");
-//             driver.FindElement(By.XPath("(//a[contains(text(),'Módulo de controle de legislações do Legis')])[2]")).Click();
-//             driver.FindElement(By.XPath("(//a[contains(text(),'Consultar Legislação')])[2]")).Click();
-//             driver.FindElement(By.XPath("(//input[@type='text'])[5]")).Click();
-//             driver.FindElement(By.XPath("//div[@id='a15b7dbf955f-62']/span")).Click();
-//             driver.FindElement(By.XPath("//div[@id='collapseTwo']/div/form/div[2]/button")).Click();
-//         }
-//         private bool IsElementPresent(By by)
-//         {
-//             try
-//             {
-//                 driver.FindElement(by);
-//                 return true;
-//             }
-//             catch (NoSuchElementException)
-//             {
-//                 return false;
-//             }
-//         }
-        
-//         private bool IsAlertPresent()
-//         {
-//             try
-//             {
-//                 driver.SwitchTo().Alert();
-//                 return true;
-//             }
-//             catch (NoAlertPresentException)
-//             {
-//                 return false;
-//             }
-//         }
-        
-//         private string CloseAlertAndGetItsText() {
-//             try {
-//                 IAlert alert = driver.SwitchTo().Alert();
-//                 string alertText = alert.Text;
-//                 if (acceptNextAlert) {
-//                     alert.Accept();
-//                 } else {
-//                     alert.Dismiss();
-//                 }
-//                 return alertText;
-//             } finally {
-//                 acceptNextAlert = true;
-//             }
-//         }
-//     }
-// }
+        private void LimparMemoria()
+        {
+            string comando;
+            comando = "tasklist | grep chromedriver";
+            System.Diagnostics.Process.Start("CMD.exe", comando);
+
+            comando = "taskkill /F /IM chromedriver.exe /T";
+            System.Diagnostics.Process.Start("CMD.exe", comando);
+        }
+
+        [SetUp]
+        public void SetupTest()
+        {
+            //this.LimparMemoria();
+            driver = new ChromeDriver(ChromeDriverService.CreateDefaultService(Environment.CurrentDirectory));
+
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            this.wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(10));
+
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        public enum Selector
+        {
+            ID,
+            CLASS,
+            CSS,
+            XPATH,
+            LINK,
+        }
+
+        public IWebElement FindBy(Selector selector, string refs)
+        {
+            IWebElement toReturn;
+            try
+            {
+                switch (selector)
+                {
+                    case Selector.ID:
+                        toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.Id(refs)));
+                        break;
+                    case Selector.CLASS:
+                        toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.ClassName(refs)));
+                        break;
+                    case Selector.CSS:
+                        toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.CssSelector(refs)));
+                        break;
+                    case Selector.XPATH:
+                        // toReturn = wait.Until(ExpectedConditions.ElementExists(By.XPath(refs)));
+                        toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.XPath(refs)));
+                        break;
+                    case Selector.LINK:
+                        toReturn = this.wait.Until(ExpectedConditions.ElementExists(By.LinkText(refs)));
+                        break;
+                    default:
+                        toReturn = null;
+                        break;
+                }
+
+            }
+            catch (System.Exception)
+            { toReturn = null; }
+
+            Thread.Sleep(150);
+            return toReturn;
+        }
+
+        public void Login()
+        {
+            driver.Navigate().GoToUrl("http://localhost:4200/");
+
+            IWebElement username = FindBy(Selector.ID, "username");
+            username.Click();
+            username.Clear();
+            username.SendKeys("05510151447");
+
+            IWebElement password = FindBy(Selector.ID, "password");
+            password.Click();
+            password.Clear();
+            password.SendKeys("tce@123");
+
+            FindBy(Selector.XPATH, "(//input[@type='text'])[2]").Click();
+            FindBy(Selector.XPATH, "/html/body/app-root/tce-login/div/form/div[3]/ng-select/ng-dropdown-panel/div/div[2]/div[1]").Click();
+
+            FindBy(Selector.ID, "idEntrarLogin").Click();
+        }
+
+        // [Test]
+        // public void testeBuscaOrgaoCadastranteDataInicioVigencia()
+        // {
+        //     Login();
+
+        //     //* go to module
+        //     FindBy(Selector.LINK, "Módulo de controle de legislações do Legis").Click();
+        //     FindBy(Selector.ID, "LegislacaoConsulta").Click();
+        //     Thread.Sleep(16000);
+
+        //     //select Numero Norma
+        //     IWebElement numeroNorma = FindBy(Selector.ID, "numeroNorma");
+        //     numeroNorma.Click();
+        //     numeroNorma.Clear();
+        //     numeroNorma.SendKeys("123456789");
+
+        //     //select orgao cadastrante
+        //     FindBy(Selector.ID, "select_idOrgaoCadastrante").Click();
+        //     FindBy(Selector.XPATH, "/html/body/app-root/app-dashboard/div/div/main/app-legislacao-consulta/div[1]/div[2]/div/form/div[1]/div[8]/ng-select/ng-dropdown-panel/div/div[2]/div[345]").Click();
+
+
+        //     //enviar
+        //     FindBy(Selector.XPATH,"//div[@id='collapseTwo']/div/form/div[2]/button").Click();
+        //     Thread.Sleep(5000);
+
+        //     //validar 
+
+        // }
+
+        [Test]
+        public void testeBuscaNormasAnoEsfera()
+        {
+            Login();
+
+            //* go to module
+            FindBy(Selector.LINK, "Módulo de controle de legislações do Legis").Click();
+            FindBy(Selector.ID, "LegislacaoConsulta").Click();
+            Thread.Sleep(16000);
+
+            //select ano
+            IWebElement anoNorma = FindBy(Selector.ID, "anoNorma");
+            anoNorma.Click();
+            anoNorma.Clear();
+            anoNorma.SendKeys("2019");
+
+            //select esferaGovernamental
+            FindBy(Selector.ID, "idEsferaGovernamental").Click();
+            FindBy(Selector.XPATH, "/html/body/ng-dropdown-panel/div/div[2]/div[1]").Click();
+
+            //enviar
+            FindBy(Selector.XPATH,"//div[@id='collapseTwo']/div/form/div[2]/button").Click();
+            Thread.Sleep(5000);
+
+            //excluir
+            FindBy(Selector.ID,"legislacao-excluir-1").Click();
+            
+
+            FindBy(Selector.XPATH,"/html/body/div[2]/div/div[3]/button[1]").Click();
+            Thread.Sleep(400);    
+            Assert.AreEqual(FindBy(Selector.ID,"swal2-title").Text, "Sucesso");
+
+        }
+
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
